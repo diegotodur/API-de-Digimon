e5acc0a3f0c555d181e2c3bce6ec8d4b33f6b0dc
DiegoTorres <miscelanneo@gmail.com>
2023-04-21 19:29:57 -0400

    add files

---
+++ README.md
@@ -0,0 +1,23 @@
+# DIGI SEARCH
+
+Busqueda de Digimones bajo el consumo de su API.
+
+### Documentacion 🔧
+
+
+_Documentacion_
+
+```
+https://digimon-api.vercel.app/
+```
+
+## Construido con 🛠️
+
+* [Javascript](https://www.javascript.com/) 
+* [Bootstrap](https://getbootstrap.com/docs/5.3/getting-started/introduction/) - La libreria mas popular de HTML, CSS y JS en el mundo.
+
+
+## Autores ✒️
+
+* **Diego** - [itsmisce](https://github.com/itsmisce)
+
---
+++ SRC/JS/index.js
@@ -0,0 +1,47 @@
+
+document.title= "DIGI API"
+const BASE_URL =  "https://www.digi-api.com/api/v1/digimon/"
+
+function digiAPI () {
+    let nombreDigimon = document.getElementById("input_name").value;
+
+    let tarjetaError =  `
+            
+        <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded position-absolute top-50 start-50 translate-middle rounded-4 bg-info-subtle card-small" ">
+              <img src="./SRC/IMG/error.jpg" class="card-img-top" alt="...">
+               <div class="card-body">
+                     <div class="text-center">
+                       <p id="diginame" class="card-text text-light ">ERROR</p>
+                       <p id="digilevel" class="card-text text-light ">Digimon no Encontrado, Intente Nuevamente</p>
+                     </div>
+               </div>
+            </div> 
+        `;
+
+    fetch(`${BASE_URL}${nombreDigimon}`)
+        .then(response => response.json())
+        .then(data => {
+            let imgDigi = data.images[0].href
+            let nmDigi = data.name
+            let nvlDigi = data.levels[0].level
+
+            let tarjeta = `
+                <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded position-absolute top-50 start-50 translate-middle rounded-4 bg-info-subtle card-small" ">
+                    <img src="${imgDigi}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="text-center">
+                          <p id = "diginame" class="card-text text-light ">Nombre: ${nmDigi}</p>
+                          <p id = "digilevel" class="card-text text-light ">Nivel: ${nvlDigi}</p>
+                        </div>
+                    </div>
+                </div> 
+            `;
+            document.title= nmDigi;
+            document.getElementById("card").innerHTML = tarjeta;
+        })
+        .catch(error => {
+            document.getElementById("card").innerHTML = tarjetaError;
+        });
+}
+
+
