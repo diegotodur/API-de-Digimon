2249f1f7462a49699fa861a18ca532e8468b6868
DiegoTorres <miscelanneo@gmail.com>
2023-05-05 14:58:09 -0400

    card style

--- SRC/JS/index.js
+++ SRC/JS/index.js
@@ -1,45 +1,79 @@
 
 document.title= "DIGI API"
 const BASE_URL =  "https://www.digi-api.com/api/v1/digimon/"
+let digiButton = document.getElementById("digiboton");
 
-function digiAPI () {
-    let nombreDigimon = document.getElementById("input_name").value;
-
-    let tarjetaError =  `
-            
-        <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded position-absolute top-50 start-50 translate-middle rounded-4 bg-info-subtle card-small" ">
-              <img src="./SRC/IMG/error.jpg" class="card-img-top" alt="...">
-               <div class="card-body">
-                     <div class="text-center">
-                       <p id="diginame" class="card-text text-light ">ERROR</p>
-                       <p id="digilevel" class="card-text text-light ">Digimon no Encontrado, Intente Nuevamente</p>
-                     </div>
-               </div>
-            </div> 
+ let tarjetaloader = `
+        <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded-4 position-absolute top-50 start-50 translate-middle rounded-4 bg-info-subtle card-small" style="width: 22rem; height: 30rem; border-radius: 20px;">
+        <span class="loader position-absolute top-50 start-50 translate-middle"></span>
+        <div class="card-body">
+              <div class="text-center">
+                <p id="diginame" class="card-text text-light"><strong>Developed with ❤️</strong></p>
+                <p id="digilevel" class="card-text text-light"><strong>by itsmisce</strong></p>
+              </div>
+        </div>
+        </div> 
         `;
-
+digiButton.addEventListener('click', function() {
+    let nombreDigimon = document.getElementById("input_name").value;
+  
+    let tarjetaError =  `
+    <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded-4 position-absolute top-50 start-50 translate-middle rounded-4 bg-info-subtle card-small fade-in" style="width: 22rem; height: 30rem; border-radius: 20px;">
+    <img src="./SRC/IMG/error.jpg" class="card-img-top" alt="..." height="350px">
+        <div class="card-body">
+          <div class="text-center">
+            <p id="diginame" class="card-text text-light">ERROR</p>
+            <p id="digilevel" class="card-text text-light">Digimon no Encontrado, Intente Nuevamente</p>
+          </div>
+        </div>
+      </div> 
+    `;
+  
     fetch(`${BASE_URL}${nombreDigimon}`)
-        .then(response => response.json())
-        .then(data => {
-            let imgDigi = data.images[0].href
-            let nmDigi = data.name
-            let nvlDigi = data.levels[0].level
+      .then(response => response.json())
+      .then(data => {
+        let imgDigi = data.images[0].href
+        let nmDigi = data.name
+        let nvlDigi = data.levels[0].level
+  
+        let tarjetaloader = `
+        <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded-4 position-absolute top-50 start-50 translate-middle rounded-4 bg-info-subtle card-small" style="width: 22rem; height: 30rem; border-radius: 20px;">
+        <span class="loader position-absolute top-50 start-50 translate-middle"></span>
+        <div class="card-body">
+              <div class="text-center">
+                <p id="diginame" class="card-text text-light"><strong>Developed with ❤️</strong></p>
+                <p id="digilevel" class="card-text text-light"><strong>by itsmisce</strong></p>
+              </div>
+        </div>
+        </div> 
+        `;
+  
+        let tarjeta = `
+        <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded-4 position-absolute top-50 start-50 translate-middle rounded-4 bg-info-subtle card-small fade-in" style="width: 22rem; height: 30rem; border-radius: 20px;">
+        <img src="${imgDigi}" class="card-img-top fade-in " alt="..." height="350px">
+            <div class="card-body">
+              <div class="text-center">
+                <p id="diginame" class="card-text text-light"><strong>Nombre:</strong> ${nmDigi}</p>
+                <p id="digilevel" class="card-text text-light"><strong>Nivel:</strong> ${nvlDigi}</p>
+              </div>
+            </div>
+          </div> 
+        `;
+        document.title= nmDigi;
+  
+        document.getElementById("card").innerHTML = tarjetaloader;
+        
+        setTimeout(() => {
+          document.getElementById("card").innerHTML = tarjeta; 
+        }, 1000); // 3000 milliseconds = 3 seconds
+      })
+      .catch(error => {
 
-            let tarjeta = `
-                <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded position-absolute top-50 start-50 translate-middle rounded-4 bg-info-subtle card-small" ">
-                    <img src="${imgDigi}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <div class="text-center">
-                          <p id = "diginame" class="card-text text-light ">Nombre: ${nmDigi}</p>
-                          <p id = "digilevel" class="card-text text-light ">Nivel: ${nvlDigi}</p>
-                        </div>
-                    </div>
-                </div> 
-            `;
-            document.title= nmDigi;
-            document.getElementById("card").innerHTML = tarjeta;
-        })
-        .catch(error => {
+        document.getElementById("card").innerHTML = tarjetaloader;
+        setTimeout(() => {
             document.getElementById("card").innerHTML = tarjetaError;
-        });
-}
+        }, 1000); // 3000 milliseconds = 3 seconds
+      });
+
+})
+    
